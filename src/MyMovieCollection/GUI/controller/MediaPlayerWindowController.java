b3818869b435efd05fcbe62ee8bad10bb5346c72
AbdilK <43569235+AbdilK@users.noreply.github.com>
2019-01-17 10:35:39 +0100

    fixed moviepath

--- src/MyMovieCollection/GUI/controller/MediaPlayerWindowController.java
+++ src/MyMovieCollection/GUI/controller/MediaPlayerWindowController.java
@@ -102,7 +102,7 @@ public class MediaPlayerWindowController implements Initializable
         movieView.setPreserveRatio(false); 
 
      
-        String path = moviePath + ".mp4";
+        String path = moviePath;
 
         movieMedia = new Media(new File(path).toURI().toString());
 
